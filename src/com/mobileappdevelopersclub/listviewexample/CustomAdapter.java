3add6cee2ede656808a9b58f5958b5a8be5bf5dc
Vinnie Vendemia <vvendemia1@gmail.com>
2013-12-02 18:17:22 -0500

    changes from last meeting

--- src/com/mobileappdevelopersclub/listviewexample/CustomAdapter.java
+++ src/com/mobileappdevelopersclub/listviewexample/CustomAdapter.java
@@ -7,16 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CustomAdapter extends ArrayAdapter<String> {
+import com.squareup.picasso.Picasso;
+
+public class CustomAdapter extends ArrayAdapter<Contact> {
 
 	
 	private Context context;
-	private List<String> objects;
+	private List<Contact> objects;
 	private LayoutInflater mInflater;
 	
-	public CustomAdapter(Context context, int resource, List<String> objects) {
+	public CustomAdapter(Context context, int resource, List<Contact> objects) {
 		super(context, resource, objects);
 		this.context = context;
 		this.objects = objects;
@@ -36,7 +39,18 @@ public class CustomAdapter extends ArrayAdapter<String> {
 		convertView = View.inflate(context, R.layout.simple_adapter_layout, null);
 		
 		TextView name = (TextView) convertView.findViewById(R.id.name);
-		name.setText(objects.get(position));
+		ImageView pic = (ImageView) convertView.findViewById(R.id.picture);
+		TextView phone = (TextView) convertView.findViewById(R.id.phone);
+		TextView email = (TextView) convertView.findViewById(R.id.email);
+
+		Contact c = objects.get(position);
+		
+		name.setText(c.getName());
+		
+		phone.setText(c.getPhoneNumber());
+		email.setText(c.getEmail());
+		
+		Picasso.with(context).load(c.getPicture()).into(pic);
 		
 		return convertView;
 	}
